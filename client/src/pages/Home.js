31af9e1c82149bad96296b6553c8ed843aefddf3
Michelle Huang <1524michelle@berkeley.edu>
2024-01-11 22:34:14 -0800

    make contributor signup page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
         
             <h1>never double-gift again.</h1>
-            <p>Create a personalized wishlist, share it with friends, and let them choose the perfect gifts for you!</p>
+            <p>create a personalized wishlist, share it with friends, and let them choose the perfect gifts for you!</p>
             <NameForm type="owner" wishlistUuid="" />
 
         </div>
--- client/src/pages/WishlistContributorsSignup.js
+++ client/src/pages/WishlistContributorsSignup.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NameForm } from '../components';
+import { useParams } from 'react-router-dom';
 
-const WishlistContributorsSignup = () => {
+const WishlistContributorsSignup = ({ owner_name }) => {
+    const { wishlistUuid } = useParams();
     return (
         <div>
         
-            <h1>WishlistContributorsSignup</h1>
+        <h1>welcome to {owner_name}'s wishlist!</h1>
+        <p>enter your name to begin checking items off {owner_name}'s wishlist!</p>
+        <NameForm type="contributor" wishlistUuid={`${wishlistUuid}`} />
             
         </div>
     );
